e61e3f88f255e623b3483dca1d0f5a1f2337a570
Dawid Linek <dawid@linek.dev>
2024-09-01 10:15:51 +0200

    fix: build errors

--- app/hooks/handle_search_query.ts
+++ app/hooks/handle_search_query.ts
@@ -5,30 +5,22 @@ import { LucidModel, ModelAttributes } from '@adonisjs/lucid/types/model'
  * Based on object of params filter query including in, from, to, like options
  */
 export const handleSearchQuery = <T extends LucidModel>() =>
-  scope(
-    (
-      query,
-      params: Partial<Record<
-        keyof ModelAttributes<InstanceType<T>>,
-        any
-      >>
-    ) => {
-      for (const param in params) {
-        const value = params[param]
-        if (!value) continue
-        if (Array.isArray(value)) {
-          query.whereIn(param, value)
-        } else if (typeof value === 'object') {
-          if (value.from) query.where(param, '>=', value.from)
-          if (value.to) query.where(param, '<=', value.to)
+  scope((query, params: Partial<Record<keyof ModelAttributes<InstanceType<T>>, any>>) => {
+    for (const param in params) {
+      const value = params[param] as null | string[] | string | { from: string; to: string }
+      if (!value) continue
+      if (Array.isArray(value)) {
+        query.whereIn(param, value)
+      } else if (typeof value === 'object') {
+        if ((value as { from: string }).from) query.where(param, '>=', value.from)
+        if (value?.to) query.where(param, '<=', value.to)
+      } else {
+        // number and boolean filters does not work with whereLike
+        if (Number.isNaN(Number(value)) && !['true', 'false'].includes(value)) {
+          query.whereRaw(`cast(${param} as text) ILIKE '${value}'`)
         } else {
-          // number and boolean filters does not work with whereLike
-          if (Number.isNaN(Number(value)) && !['true', 'false'].includes(value)) {
-            query.whereRaw(`cast(${param} as text) ILIKE '${value}'`)
-          } else {
-            query.where(param, value)
-          }
+          query.where(param, value)
         }
       }
     }
-  )
+  })
